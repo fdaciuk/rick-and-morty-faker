f134624c94157d9198f13994da7432b836c002f0
Fernando Daciuk <f.daciuk@gmail.com>
2021-12-16 08:09:04 -0300

    get chars from page (filesystem implementation)

--- lib/services/filesystem/char/get.ex
+++ lib/services/filesystem/char/get.ex
@@ -1,5 +1,6 @@
 defmodule RMFaker.Services.Filesystem.Char.Get do
   @not_found {:error, "Character not found"}
+  @page_not_found {:error, "There is nothing here"}
 
   def get_chars_by_id(ids) do
     result = ids
@@ -10,29 +11,39 @@ defmodule RMFaker.Services.Filesystem.Char.Get do
     {:ok, result}
   end
 
-  def remove_errors(result) do
+  def get_char_by_id(id) do
+    per_page = 20
+
+    String.to_integer("#{id}") / per_page
+    |> ceil()
+    |> get_char_from_disk_by_page(id)
+  end
+
+  def get_chars_from_page(page) do
+    page = if is_integer(page), do: page, else: 1
+    get_file_from_disk_by_page(page)
+  end
+
+  defp remove_errors(result) do
     case result do
       {:ok, _} -> true
       {:error, _} -> false
     end
   end
 
-  def get_char_by_id(id) do
-    per_page = 20
-
-    String.to_integer("#{id}") / per_page
-    |> ceil()
-    |> get_file_from_disk_by_page(id)
-  end
-
-  defp get_file_from_disk_by_page(page, id) do
+  defp get_file_from_disk_by_page(page) do
     case File.read("./data/page-#{page}.json") do
-      {:ok, data} -> Jason.decode(data) |> get_char_from_file(id)
-      {:error, _} -> @not_found
+      {:ok, data} -> Jason.decode(data)
+      {:error, _} -> @page_not_found
     end
   end
 
-  defp get_char_from_file({:error, _} = error, _id), do: error
+  defp get_char_from_disk_by_page(page, id) do
+    case get_file_from_disk_by_page(page) do
+      {:ok, _} = result -> get_char_from_file(result, id)
+      {:error, _} = error -> error
+    end
+  end
 
   defp get_char_from_file({:ok, %{"results" => results}}, id) do
     results
